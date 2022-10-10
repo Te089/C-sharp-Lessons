e4649fd0628c5ac5bc048af502995e82b6d37eea
Sergey <rsa89@bk.ru>
2022-10-10 18:12:37 +0600

    initial commit

---
+++ Lesson 9/task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Prompt(string messege)
+{
+Console.Write(messege);
+string strValue = Console.ReadLine() ?? "0";
+bool isNumber = int.TryParse(strValue, out int value);
+if (isNumber)
+{
+return value;
+}
+throw new Exception("Данное значение не возможно преобразовать в число");
+}
+
+void ShowNumbers(int number)
+{
+if (number == 0){return;}
+System.Console.Write($"{number} ");
+ShowNumbers(number - 1);
+
+
+}
+int digit = Prompt("Введите число: ");
+ShowNumbers(digit);
